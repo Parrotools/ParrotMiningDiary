b2ff4647acaf029b4dd5791f776f3a32597ae51a
Parrotools <2261216827@qq.com>
2025-10-02 10:39:17 +0800

    Drawer Animation Deployed

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 console.log('Happy developing ✨')
+// 获取元素
+const moreMenu = document.getElementById('moremenu');
+const moreContent = document.getElementById('morecontent');
+
+// 点击事件处理
+moreMenu.addEventListener('click', () => {
+    // 切换显示/隐藏状态
+    moreContent.classList.toggle('show');
+});
+
